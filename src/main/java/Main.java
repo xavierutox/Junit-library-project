46c40876f2a9b717775946e5899678409069d328
Javier Pérez <javier.perez@zippedi.com>
2022-12-13 20:37:59 -0300

    faltaba la clase main

---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        LibraryConsoleSystem libraryConsoleSystem = new LibraryConsoleSystem();
+        libraryConsoleSystem.console();
+    }
+    
+}
